e249f1ceaa172511bd6345811ee03d44e5553e62
dduoming <dltndudvldzm@naver,com>
2021-11-05 19:01:27 +0900

    6번째 시험 망함

--- test5.py
+++ test5.py
@@ -1,88 +1,54 @@
-from datetime import datetime
-
-from flask import Flask, render_template, jsonify, request
-from pymongo import MongoClient
-import os
-
-
-import jwt
-import datetime
-import hashlib
 from datetime import datetime, timedelta
+from functools import wraps
+
+from flask import Flask, render_template, jsonify, request, Response, g
+from pymongo import MongoClient
+import jwt
+import bcrypt
+
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client.dbStock
+secret = "secrete"
+algorithm = "HS256"
 
-SECRET_KEY = 'sooyoung'
+def login_check(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        access_token = request.headers.get("Authorization")
+        if access_token is not None:
+            try:
+                payload = jwt.decode(access_token, secret, "HS256")
+            except jwt.InvalidTokenError:
+                return Response(status=401)
+
+            if payload is None:
+                return Response(status=401)
+
+            user_id = payload["id"]
+            g.user_id = user_id
+            g.user = get_user_info(user_id)
+        else:
+            g.user_id = "비회원"
+            g.user = None
+            print("access_token is empty")
+
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def get_user_info(user_id):
+    return db.user.find_one({"id": user_id})
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/index')
-def index2():
-    # return render_template('1page.html')
-    token_receive = request.cookies.get('mytoken')
-    print(token_receive)
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-        user_info = db.users.find_one({"username": payload["id"]}, )
-        username = user_info['username']
-        popular = list(db.sample.find({}, {'_id': False}).sort([("heart_count", -1)]).limit(5))
-        print(popular)
-        return render_template('index.html', username=username, token=token, places=popular)
-
-
-    except:
-        print('실패')
-        return render_template('index.html')
-
-@app.route('/sign_up/save', methods=['POST'])
-def sign_up():
-    username_receive = request.form['userId']
-    password_receive = request.form['userPw']
-    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    doc = {
-        "username": username_receive,  # 아이디
-        "password": password_hash,  # 비밀번호
-
-    }
-    db.users.insert_one(doc)
-    return render_template('index.html')
-
-#
-# # 아이디 중복확인
-# @app.route('/sign_up/check_dup', methods=['POST'])
-# def check_dup():
-#     username_receive = request.form['username_give']
-#     exists = bool(db.users.find_one({"username": username_receive}, {'_id': False}))
-#     return jsonify({'result': 'success', 'exists': exists})
-
-
-@app.route('/sign_in', methods=['POST'])
-def sign_in():
-    # 로그인
-    username_receive = request.form['username_give']
-    password_receive = request.form['password_give']
-
-    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
-    print(username_receive,password_receive)
-    if result is not None:
-        payload = {
-            'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=700)  # 로그인 24시간 유지
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-        username=payload['id']
-        return jsonify({'result': 'success', 'token': token,'msg':'로그인이 되었습니다','username':username})
-    # 찾지 못하면
-    else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+    return render_template('test5.html')
 
 
 @app.route('/article', methods=['POST'])
+@login_check
 def save_post():
     title = request.form.get('title')
     content = request.form.get('content')
@@ -97,14 +63,16 @@ def save_post():
         'title': title,
         'content': content,
         'read_count': 0,
+        'writer': (g.user_id if hasattr(g, 'user_id') else ''),
         'reg_date': datetime.now()
     }
     db.article.insert_one(post)
     return {"result": "success"}
 
 
-@app.route('/articles', methods=['GET'])
-def get_posts():
+@app.route('/articles/<type>', methods=['GET'])
+@login_check
+def get_posts(type):
     order = request.args.get('order')
     per_page = request.args.get('perPage')
     cur_page = request.args.get('curPage')
@@ -113,9 +81,12 @@ def get_posts():
     if search_title is not None:
         search_condition = {"title": {"$regex": search_title}}
 
+    if type == 'my':
+        search_condition['writer'] = g.user_id
+
     limit = int(per_page)
     skip = limit * (int(cur_page) - 1)
-    total_count = db.article.find(search_condition).count()
+    total_count = db.article.count_documents(search_condition)
     total_page = int(total_count / limit) + (1 if total_count % limit > 0 else 0)
 
     if order == "desc":
@@ -135,7 +106,8 @@ def get_posts():
         "curPage": cur_page,
         "searchTitle": search_title
     }
-
+    print(articles)
+    print(paging_info)
     return jsonify({"articles": articles, "pagingInfo": paging_info})
 
 
@@ -159,17 +131,46 @@ def update_post():
     title = request.form.get('title')
     content = request.form.get('content')
     db.article.update_one({'idx': int(idx)}, {'$set': {'title': title, 'content': content}})
-    return {"result": "success"}
+    return jsonify({"result": "success"})
 
 
 @app.route('/article/<idx>', methods=['PUT'])
 def update_read_count(idx):
-    #idx를 찾아 read_count에 1더하기
     db.article.update_one({'idx': int(idx)}, {'$inc': {'read_count': 1}})
     article = db.article.find_one({'idx': int(idx)}, {'_id': False})
     return jsonify({"article": article})
 
 
+@app.route('/join', methods=['POST'])
+def create_user():
+    id = request.form.get('id')
+    pwd = request.form.get('pwd')
+    if get_user_info(id) is not None:
+        return Response(status=423)
+
+    db.user.insert_one({"id": id, "pwd": bcrypt.hashpw(pwd.encode('UTF-8'), bcrypt.gensalt())})
+    return jsonify({"result": "success"})
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    id = request.form.get('id')
+    pwd = request.form.get('pwd')
+    user = db.user.find_one({"id": id})
+
+    if get_user_info(id) is None:
+        return Response(status=401)
+
+    if bcrypt.checkpw(pwd.encode('utf-8'), user['pwd']):
+        payload = {
+            "id": id,
+            "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload, secret, "HS256")
+        return jsonify({"result": "success", "token": token})
+    else:
+        return Response(status=401)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000)
